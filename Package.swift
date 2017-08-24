7b2e703eb896a7ced4f8c4409abb8ff2e4057cbf
Lev Sokolov <sokolov.lev95@gmail.com>
2017-08-24 18:44:46 +0300

    Update libwebp dependency

--- Package.swift
+++ Package.swift
@@ -2,7 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "WebP",
-    dependencies: [
-        .Package(url: "https://github.com/ainame/CWebP.git", majorVersion: 0, minor: 6)
-    ]
+    targets: [Target(name: "libwebp", dependencies:["objc"])]
 )
